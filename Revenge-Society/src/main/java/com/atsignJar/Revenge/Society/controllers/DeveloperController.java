06732f550f11d7159734069323467eecb834788b
Jeremy <jeremyeparker@yahoo.com>
2021-12-07 18:54:22 -0500

    add CRUD routes

--- Revenge-Society/src/main/java/com/atsignJar/Revenge/Society/controllers/DeveloperController.java
+++ Revenge-Society/src/main/java/com/atsignJar/Revenge/Society/controllers/DeveloperController.java
@@ -1,19 +1,15 @@
-package com.geekylikes.app.controllers;
+package com.atsignJar.Revenge.Society.controllers;
 
-import com.geekylikes.app.models.avatar.Avatar;
-import com.geekylikes.app.models.developer.Developer;
-import com.geekylikes.app.models.geekout.Geekout;
-import com.geekylikes.app.repositories.AvatarRepository;
-import com.geekylikes.app.repositories.DeveloperRepository;
-import com.geekylikes.app.repositories.GeekoutRepository;
+import com.atsignJar.Revenge.Society.models.developer.Developer;
+import com.atsignJar.Revenge.Society.repositories.DeveloperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+
 
 @CrossOrigin
 @RestController
@@ -22,77 +18,26 @@ public class DeveloperController {
     @Autowired
     private DeveloperRepository repository;
 
-    @Autowired
-    private AvatarRepository avatarRepository;
-
-    @Autowired
-    GeekoutRepository geekoutRepository;
+    @GetMapping
+    public @ResponseBody List<Developer> getDevelopers(){return repository.findAll();}
 
     @GetMapping
-    public @ResponseBody List<Developer> getDevelopers() {
-        return repository.findAll();
+    public @ResponseBody Developer getOneDeveloper(@PathVariable Long id){
+        return repository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
-
-    @GetMapping("/lang/{langId}")
-    public List<Developer> getDevsByLanguage(@PathVariable Long langId) {
-        return repository.findAllByLanguages_id(langId);
-    }
-
-    @GetMapping("/cohort/{cohort}")
-    public ResponseEntity<List<Developer>> getDevelopersByCohort(@PathVariable Integer cohort) {
-        return new ResponseEntity<>(repository.findAllByCohort(cohort, Sort.by("name")), HttpStatus.OK);
-    }
-
-    @GetMapping("/likes/{devId}")
-    public List<Geekout> getApprovedGeekouts(@PathVariable Long devId) {
-        return geekoutRepository.findAllByApprovals_developer_id(devId);
-    }
-
-    @GetMapping("/{id}")
-    public @ResponseBody Developer getOneDeveloper(@PathVariable Long id) {
-        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-    }
-
 
     @PostMapping
-    public ResponseEntity<Developer> createDeveloper(@RequestBody Developer newDeveloper) {
-        return new ResponseEntity<>(repository.save(newDeveloper), HttpStatus.CREATED);
-    }
-
-    @PostMapping("/photo")
-    public Developer addPhoto(@RequestBody Developer dev) {
-        Developer developer = repository.findById(dev.getId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-        // check if developer has an avatar and if so, delete or modify existing avatar before creating new.
-        if (developer.getAvatar() != null) {
-            Avatar avatar = developer.getAvatar();
-            avatar.setUrl(dev.getAvatar().getUrl());
-            avatarRepository.save(avatar);
-            return developer;
-        }
-        Avatar avatar = avatarRepository.save(dev.getAvatar());
-        developer.setAvatar(avatar);
-        return repository.save(developer);
-
-    }
-
-    @PutMapping("/language")
-    public Developer addLanguage(@RequestBody Developer updates) {
-        Developer developer = repository.findById(updates.getId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-
-        developer.languages.addAll(updates.languages);
-        return repository.save(developer);
+    public ResponseEntity<Developer> createDeveloper(@RequestBody Developer newDeveloper){
+        return new ResponseEntity<>(repository.save(newDeveloper),HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public @ResponseBody Developer updateDeveloper(@PathVariable Long id, @RequestBody Developer updates) {
         Developer developer = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
-//        updates.setId(developer.getId());
-//        return repository.save(updates);
         if (updates.getName() != null) developer.setName(updates.getName());
         if (updates.getEmail() != null) developer.setEmail(updates.getEmail());
         if (updates.getCohort() != null) developer.setCohort(updates.getCohort());
-        if (updates.languages != null) developer.languages = updates.languages;
 
         return repository.save(developer);
     }
@@ -102,6 +47,4 @@ public class DeveloperController {
         repository.deleteById(id);
         return new ResponseEntity<>("Deleted", HttpStatus.OK);
     }
-
-
 }
