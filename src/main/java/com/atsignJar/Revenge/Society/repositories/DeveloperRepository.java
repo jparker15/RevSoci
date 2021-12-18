3a509808b57a8090674019fae841acd2466ba728
Jermy <jeremyeparker@yahoo.com>
2021-12-18 17:10:16 -0500

    add findByCohort method for JPA Repo

--- src/main/java/com/atsignJar/Revenge/Society/repositories/DeveloperRepository.java
+++ src/main/java/com/atsignJar/Revenge/Society/repositories/DeveloperRepository.java
@@ -1,7 +1,15 @@
 package com.atsignJar.Revenge.Society.repositories;
 
 import com.atsignJar.Revenge.Society.models.developer.Developer;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
+
 public interface DeveloperRepository extends JpaRepository<Developer,Long> {
+
+    List<Developer> findAllByCohort(Integer cohort, Sort sort);
+
+
 }
