86d6219d1241197931887aaa971103c0db5064c1
Jermy <jeremyeparker@yahoo.com>
2021-12-18 17:09:37 -0500

    add getbyCohort route

--- src/main/java/com/atsignJar/Revenge/Society/controllers/DeveloperController.java
+++ src/main/java/com/atsignJar/Revenge/Society/controllers/DeveloperController.java
@@ -3,6 +3,7 @@ package com.atsignJar.Revenge.Society.controllers;
 import com.atsignJar.Revenge.Society.models.developer.Developer;
 import com.atsignJar.Revenge.Society.repositories.DeveloperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,13 +19,23 @@ public class DeveloperController {
     @Autowired
     private DeveloperRepository repository;
 
+    //Get All
     @GetMapping
     public @ResponseBody List<Developer> getDevelopers(){return repository.findAll();}
 
+    //Get by ID
     @GetMapping("/{id}")
     public @ResponseBody Developer getOneDeveloper(@PathVariable Long id){
         return repository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
+
+    //Get by Cohort
+    @GetMapping("/cohort/{cohort}")
+    public ResponseEntity<List<Developer>> getDeveloperByCohort(@PathVariable Integer cohort){
+                //sorted by name
+        return new ResponseEntity<>(repository.findAllByCohort(cohort, Sort.by("name")),HttpStatus.OK);
+    }
+
 
     @PostMapping
     public ResponseEntity<Developer> createDeveloper(@RequestBody Developer newDeveloper){
