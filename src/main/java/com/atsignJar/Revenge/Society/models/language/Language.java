970ec2a50c54ab6925c57c34e15c031ae3f49ab5
Jermy <jeremyeparker@yahoo.com>
2021-12-27 17:04:08 -0500

    new model

---
+++ src/main/java/com/atsignJar/Revenge/Society/models/language/Language.java
@@ -0,0 +1,12 @@
+package com.atsignJar.Revenge.Society.models.language;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Language {
+    @Id
+    @GeneratedValue
+    Long id;
+}
