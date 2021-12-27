a4d4ad539850b96650a3ba7b24afc44e2afcb4ac
Jermy <jeremyeparker@yahoo.com>
2021-12-27 17:06:18 -0500

    findByDevId List

--- src/main/java/com/atsignJar/Revenge/Society/repositories/GeekoutRepository.java
+++ src/main/java/com/atsignJar/Revenge/Society/repositories/GeekoutRepository.java
@@ -3,6 +3,9 @@ package com.atsignJar.Revenge.Society.repositories;
 import com.atsignJar.Revenge.Society.models.geekout.Geekout;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GeekoutRepository extends JpaRepository<Geekout,Long> {
 
+    List<Geekout> findByDeveloperId(Long id);
 }
