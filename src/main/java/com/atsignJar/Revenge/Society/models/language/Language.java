fb6133d325b0840a996c80277f34534c13f5360c
Jeremy <jeremyeparker@yahoo.com>
2021-12-28 17:00:52 -0500

    flesh out rest of model, add ManyToMany with devs

--- src/main/java/com/atsignJar/Revenge/Society/models/language/Language.java
+++ src/main/java/com/atsignJar/Revenge/Society/models/language/Language.java
@@ -1,12 +1,62 @@
 package com.atsignJar.Revenge.Society.models.language;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import com.atsignJar.Revenge.Society.models.developer.Developer;
+
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Language {
     @Id
-    @GeneratedValue
-    Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String tag;
+
+    @ManyToMany
+    @JoinTable(
+            name = "developer_language",
+            joinColumns = @JoinColumn(name = "language_id"),
+            inverseJoinColumns = @JoinColumn(name = "developer_id")
+    )
+    private Set<Developer> developers;
+
+    public Language (){
+
+    }
+
+    public Language(String name, String tag, Set<Developer> developers) {
+        this.name = name;
+        this.tag = tag;
+        this.developers = developers;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+//    public Set<Developer> getDevelopers() {
+//        return developers;
+//    }
+
 }
