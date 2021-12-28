ecbcaafd75030691112061ec3a70a32a9fef5289
Jeremy <jeremyeparker@yahoo.com>
2021-12-28 16:57:11 -0500

    new repo

--- src/main/java/com/atsignJar/Revenge/Society/repositories/LanguageRepository.java
+++ src/main/java/com/atsignJar/Revenge/Society/repositories/LanguageRepository.java
@@ -1,2 +1,7 @@
-package com.atsignJar.Revenge.Society.repositories;public interface LanguageRepository {
+package com.atsignJar.Revenge.Society.repositories;
+
+import com.atsignJar.Revenge.Society.models.language.Language;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LanguageRepository extends JpaRepository<Language,Long> {
 }
