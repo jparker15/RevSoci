f795ea1ad219fe08084ee978a4140cb535bef990
Jeremy <jeremyeparker@yahoo.com>
2022-01-03 23:52:53 -0500

    f

--- src/main/java/com/atsignJar/Revenge/Society/RevengeSocietyApplication.java
+++ src/main/java/com/atsignJar/Revenge/Society/RevengeSocietyApplication.java
@@ -9,5 +9,5 @@ public class RevengeSocietyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RevengeSocietyApplication.class, args);
 	}
-
+	//The Revenge Society are group of super-villains who formed the group in opposition to the Guild of Calamitous Intent.
 }
