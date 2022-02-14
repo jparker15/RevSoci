f67a874fc00cf966f177d929c2278acd054cd78c
Jeremy <jeremyeparker@yahoo.com>
2022-02-13 19:04:38 -0500

    i forget

--- src/main/java/com/atsignJar/Revenge/Society/models/language/DevLang.java
+++ src/main/java/com/atsignJar/Revenge/Society/models/language/DevLang.java
@@ -1,2 +1,8 @@
-package com.atsignJar.Revenge.Society.models.language;public interface DevLang {
+package com.atsignJar.Revenge.Society.models.language;
+
+public interface DevLang {
+    Long getId();
+    String getName();
+    String getTag();
+
 }
