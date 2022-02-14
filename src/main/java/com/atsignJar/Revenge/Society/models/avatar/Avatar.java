f22e4867c04769b8e052851fdccd79851e6ab40e
Jeremy <jeremyeparker@yahoo.com>
2022-02-13 19:01:18 -0500

    model for pfp

--- src/main/java/com/atsignJar/Revenge/Society/models/avatar/Avatar.java
+++ src/main/java/com/atsignJar/Revenge/Society/models/avatar/Avatar.java
@@ -1,2 +1,46 @@
-package com.atsignJar.Revenge.Society.models.avatar;public class Avatar {
+package com.atsignJar.Revenge.Society.models.avatar;
+
+import com.atsignJar.Revenge.Society.models.developer.Developer;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.*;
+
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id"
+)
+
+@Entity
+public class Avatar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String url;
+
+    public Avatar (){
+
+    }
+
+    public Avatar(String url) {
+        this.url = url;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
 }
