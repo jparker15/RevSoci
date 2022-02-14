c8ff03e203e5b88cbf6eff8784b74e21faee4e54
Jeremy <jeremyeparker@yahoo.com>
2022-02-13 19:02:14 -0500

    som

--- src/main/java/com/atsignJar/Revenge/Society/models/developer/Developer.java
+++ src/main/java/com/atsignJar/Revenge/Society/models/developer/Developer.java
@@ -1,14 +1,20 @@
 package com.atsignJar.Revenge.Society.models.developer;
 
+import com.atsignJar.Revenge.Society.models.avatar.Avatar;
 import com.atsignJar.Revenge.Society.models.geekout.Geekout;
 import com.atsignJar.Revenge.Society.models.language.Language;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.*;
+import org.apache.juli.logging.Log;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id"
+)
 
 @Entity
 public class Developer {
@@ -20,29 +26,31 @@ public class Developer {
     private String email;
     private Integer cohort;
 //    private String[] languages;
-    
-    @JsonBackReference
+
     @OneToMany
     @JoinColumn(name = "developer_id",referencedColumnName = "id")
     private List<Geekout> geekouts;
 
-    @JsonManagedReference
+
     @ManyToMany
     @JoinTable(
             name = "developer_language",
             joinColumns = @JoinColumn(name = "developer_id"),
             inverseJoinColumns = @JoinColumn(name = "language_id")
     )
-    public Set <Language> languages;
+    public Set <Language> languages = new HashSet<>();
 
+    @OneToOne
+    private Avatar avatar;
     public Developer() {}
 
-    public Developer(String name, String email, Integer cohort, List<Geekout> geekouts, Set<Language> languages) {
+    public Developer(String name, String email, Integer cohort, List<Geekout> geekouts, Set<Language> languages,Avatar avatar) {
         this.name = name;
         this.email = email;
         this.cohort = cohort;
         this.geekouts = geekouts;
         this.languages = languages;
+        this.avatar = avatar;
     }
 
     public Long getId() {
@@ -92,4 +100,13 @@ public class Developer {
     public void setGeekouts(List<Geekout> geekouts) {
         this.geekouts = geekouts;
     }
+
+    public Avatar getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(Avatar avatar) {
+        this.avatar = avatar;
+    }
+
 }
