19a3a20f754f729d7d8a58298e8e11f3190fd8a4
Jeremy <jeremyeparker@yahoo.com>
2022-02-13 19:04:54 -0500

    new repo

--- src/main/java/com/atsignJar/Revenge/Society/repositories/AvatarRepository.java
+++ src/main/java/com/atsignJar/Revenge/Society/repositories/AvatarRepository.java
@@ -1,2 +1,8 @@
-package com.atsignJar.Revenge.Society.repositories;public interface AvatarRepository {
+package com.atsignJar.Revenge.Society.repositories;
+
+import com.atsignJar.Revenge.Society.models.avatar.Avatar;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvatarRepository extends JpaRepository<Avatar,Long> {
+
 }
