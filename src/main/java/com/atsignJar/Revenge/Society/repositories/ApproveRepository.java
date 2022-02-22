f19656d1cee02512b7f77f02309871faaefedf9e
Jeremy <jeremyeparker@yahoo.com>
2022-02-22 15:06:26 -0500

    new repo for Approve

--- src/main/java/com/atsignJar/Revenge/Society/repositories/ApproveRepository.java
+++ src/main/java/com/atsignJar/Revenge/Society/repositories/ApproveRepository.java
@@ -1,2 +1,7 @@
-package com.atsignJar.Revenge.Society.repositories;public class ApproveRepository {
+package com.atsignJar.Revenge.Society.repositories;
+
+import com.atsignJar.Revenge.Society.models.approve.Approve;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApproveRepository extends JpaRepository<Approve,Long> {
 }
